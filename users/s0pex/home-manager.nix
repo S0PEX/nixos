a6e2e371cb2852493f586e96ec6cb6e3f9c6dc8c
Artur Komaristych <artur.komaristych@gmail.com>
2025-01-09 20:16:19 +0100

    feat(s0pex): set default web application to LibreWolf

--- users/s0pex/home-manager.nix
+++ users/s0pex/home-manager.nix
@@ -54,6 +54,18 @@
     enable = true;
   };
 
+  # Default applications
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "application/xhtml+xml" = "librewolf.desktop";
+      "text/html" = "librewolf.desktop";
+      "text/xml" = "librewolf.desktop";
+      "x-scheme-handler/ftp" = "librewolf.desktop";
+      "x-scheme-handler/http" = "librewolf.desktop";
+      "x-scheme-handler/https" = "librewolf.desktop";
+    };
+  };
 
   # Theme settings
   gtk = {
